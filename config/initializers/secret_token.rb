6a7804ee3e2dbab2874debc6ba6fc16df6953ff7
Sumeet Jain <sumeet@sumeetjain.com>
2013-07-23 11:29:33 -0700

    Rails the usual way, plus HAML.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paintr::Application.config.secret_token = '76ba782a9e8cf2f00ad3745c8d18563a8e47bd4583fd24cf75b61128a5f088235f6342857300731bf0cfb8d0c20d9ed274e478033dc723bf3193e1037e6e6850'
